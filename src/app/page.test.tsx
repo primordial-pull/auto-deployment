9091a16276861c25df611718b23fa1a987985757
primordial-pull <primordial.pull@gmail.com>
2026-08-07 08:37:59 +0900

    test test fail case

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,13 +1,13 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
-import Home from "./page";
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import Home from './page';
 
-describe("Home", () => {
-  it("메인 헤딩을 렌더링한다", () => {
+describe('Home', () => {
+  it('메인 헤딩을 렌더링한다', () => {
     render(<Home />);
 
     expect(
-      screen.getByRole("heading", { name: /edit the page.tsx file/i }),
-    ).toBeInTheDocument();
+      screen.getByRole('heading', { name: /edit the page.tsx file/i }),
+    ).not.toBeInTheDocument();
   });
 });
